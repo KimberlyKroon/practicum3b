3a51600b032010314d21bba77aacc35e46019716
Kimberly Kroon <321231@student.mboutrecht.nl>
2022-02-25 11:41:02 +0100

    e

---
+++ src/Cirkel.java
@@ -0,0 +1,14 @@
+public class Cirkel {
+    private int radius;
+    private int xPositie;
+    private int yPositie;
+
+    public Cirkel(int rad, int x, int y){
+        radius = rad;
+        xPositie = x;
+        yPositie = y;
+    }
+    public String toString(){
+        return "cirkel (" + xPositie + ", " + yPositie + ") met radius: " + radius;
+    }
+}
